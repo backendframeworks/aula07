a361d61642de3e1407f334244fb4f6cb7ec91c1d
backendframeworks <010120694@prof.uninassau.edu.br>
2024-10-25 16:04:39 -0300

    remove cpf validation

--- src/main/java/com/backend/aula07/dto/ProductDto.java
+++ src/main/java/com/backend/aula07/dto/ProductDto.java
@@ -4,4 +4,4 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
-public record ProductDto(@NotBlank String name, @NotNull @CPF double value) {}
+public record ProductDto(@NotBlank String name, @NotNull double value) {}
